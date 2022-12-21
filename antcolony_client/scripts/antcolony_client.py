94dc3cb529c70f8cf6d86eed600049e44049205d
balint-bg <balint@berkshiregrey.com>
2022-12-21 11:10:45 -0500

    First commit - add option for aplayer to send ants to the closest sugar neighbor.
    Also some IDE-made whitespace cleanup.

--- antcolony_client/scripts/antcolony_client.py
+++ antcolony_client/scripts/antcolony_client.py
@@ -8,11 +8,14 @@ import rospy
 import antcolony_msgs.srv
 from antcolony_msgs.msg import Action, Ant, Hive, Sugar, Order, Player, State
 
+CONF_USE_CLOSEST_NEIGHBOR = True
+
 
 class GameStateFunctions:
     """
     By design behavior tree tasks get state through an object of this class
     """
+
     def __init__(self, game_state, player_id, map_size):
         self.game_state = game_state
         self.player_id = player_id
@@ -63,8 +66,22 @@ class GameStateFunctions:
     def is_adjacent(self, x1, y1, x2, y2):
         return self.get_manhattan_distance(x1, y1, x2, y2) == 1
 
-    def generate_neighbors(self, x, y):
+    def generate_neighbors(self, x, y, ant_id=None):
         neighbors = []
+        if CONF_USE_CLOSEST_NEIGHBOR and ant_id is not None:
+            ant = self.ants[ant_id]
+            closest_neighbor = None
+            closest_distance = 1000
+            for neighbor in self.generate_neighbors(x, y):
+                distance = self.get_manhattan_distance(
+                    ant.x, ant.y, neighbor[0], neighbor[1]
+                )
+                if distance < closest_distance:
+                    closest_distance = distance
+                    closest_neighbor = neighbor
+            if closest_neighbor is not None:
+                neighbors.append(closest_neighbor)
+        # TODO: do not append the closest neighbor twice.
         if x > 0:
             neighbors.append((x - 1, y))
         if y > 0:
@@ -116,6 +133,7 @@ class GamePlayControl:
     """
     By design behavior tree tasks act on game state through this class
     """
+
     def __init__(self):
         self.actions = {}
 
@@ -139,9 +157,10 @@ class TaskState:
     """
     Enum for states of behavior tree task
     """
-    RUNNING = 'RUNNING'
-    SUCCEEDED = 'SUCCEEDED'
-    FAILED = 'FAILED'
+
+    RUNNING = "RUNNING"
+    SUCCEEDED = "SUCCEEDED"
+    FAILED = "FAILED"
 
 
 class BaseTask:
@@ -160,13 +179,13 @@ class BaseTask:
 
     def is_succeeded(self):
         return self.state == TaskState.SUCCEEDED
-    
+
     def set_succeeded(self):
         self.state = TaskState.SUCCEEDED
-    
+
     def is_failed(self):
         return self.state == TaskState.FAILED
-    
+
     def set_failed(self):
         self.state = TaskState.FAILED
 
@@ -185,31 +204,33 @@ class BaseTask:
             return
 
         self.run_impl()
-        
-        
+
+
 class GameControlTask(BaseTask):
     """
     A base class for behavior tree task with access to reading current situation and acting on it
     """
+
     def __init__(self):
         BaseTask.__init__(self)
-        self.f = None
-        self.c = None
-    
+        self.game_functions = None
+        self.game_play_control = None
+
     # Some classes may need to override this method
     def update_child_states(self, game_state_functions, game_play_control):
         pass
-    
+
     def update_state(self, game_state_functions, game_play_control):
-        self.f = game_state_functions
-        self.c = game_play_control
-        self.update_child_states(self.f, self.c)
-    
+        self.game_functions = game_state_functions
+        self.game_play_control = game_play_control
+        self.update_child_states(self.game_functions, self.game_play_control)
+
 
 class MoveAntToTargetPointTask(GameControlTask):
     """
     Until ant exists move to target point
     """
+
     def __init__(self, ant_id, target_x, target_y):
         GameControlTask.__init__(self)
         self.ant_id = ant_id
@@ -217,20 +238,25 @@ class MoveAntToTargetPointTask(GameControlTask):
         self.target_y = target_y
 
     def is_valid(self):
-        return self.f.my_ant_exists(self.ant_id) and not self.f.is_occupied(self.target_x, self.target_y)
+        return self.game_functions.my_ant_exists(
+            self.ant_id
+        ) and not self.game_functions.is_occupied(self.target_x, self.target_y)
 
     def is_succeeded(self):
-        ant_x, ant_y = self.f.get_ant_coords(self.ant_id)
+        ant_x, ant_y = self.game_functions.get_ant_coords(self.ant_id)
         return (ant_x == self.target_x) and (ant_y == self.target_y)
 
     def run_impl(self):
-        self.c.add_action(self.ant_id, self.target_x, self.target_y, Action.MOVE_ACTION)
-        
-        
+        self.game_play_control.add_action(
+            self.ant_id, self.target_x, self.target_y, Action.MOVE_ACTION
+        )
+
+
 class AttackTargetAtPointTask(GameControlTask):
     """
     Until ant exists and adjacent target exists at target coordinates attack it
     """
+
     def __init__(self, ant_id, target_x, target_y, target_id):
         GameControlTask.__init__(self)
         self.ant_id = ant_id
@@ -239,16 +265,26 @@ class AttackTargetAtPointTask(GameControlTask):
         self.target_id = target_id
 
     def is_valid(self):
-        ant_x, ant_y = self.f.get_ant_coords(self.ant_id)
-        id_at_target_coords = self.f.get_id_at_coords(self.target_x, self.target_y)
-        return self.f.my_ant_exists(self.ant_id) and self.f.is_adjacent(ant_x, ant_y, self.target_x, self.target_y) \
-            and id_at_target_coords is not None and id_at_target_coords == self.target_id
+        ant_x, ant_y = self.game_functions.get_ant_coords(self.ant_id)
+        id_at_target_coords = self.game_functions.get_id_at_coords(
+            self.target_x, self.target_y
+        )
+        return (
+            self.game_functions.my_ant_exists(self.ant_id)
+            and self.game_functions.is_adjacent(
+                ant_x, ant_y, self.target_x, self.target_y
+            )
+            and id_at_target_coords is not None
+            and id_at_target_coords == self.target_id
+        )
 
     def is_succeeded(self):
-        return not self.f.object_exists(self.target_id)
+        return not self.game_functions.object_exists(self.target_id)
 
     def run_impl(self):
-        self.c.add_action(self.ant_id, self.target_x, self.target_y, Action.ATTACK_ACTION)
+        self.game_play_control.add_action(
+            self.ant_id, self.target_x, self.target_y, Action.ATTACK_ACTION
+        )
 
 
 class SucceedAllTask(GameControlTask):
@@ -256,6 +292,7 @@ class SucceedAllTask(GameControlTask):
     Runs a list of tasks sequentially until first failure
     Succeeds if last tasks succeeded
     """
+
     def __init__(self, task_list):
         GameControlTask.__init__(self)
         self.task_list = task_list
@@ -266,11 +303,14 @@ class SucceedAllTask(GameControlTask):
             task.update_state(game_state_functions, game_play_control)
 
     def is_valid(self):
-        return self.position_index < len(self.task_list) and not self.task_list[self.position_index].has_terminal_state()
+        return (
+            self.position_index < len(self.task_list)
+            and not self.task_list[self.position_index].has_terminal_state()
+        )
 
     def is_succeeded(self):
         return self.task_list[-1].is_succeeded()
-            
+
     def run_impl(self):
         self.task_list[self.position_index].run()
         if self.task_list[self.position_index].is_succeeded():
@@ -282,6 +322,7 @@ class SucceedAnyTask(GameControlTask):
     Runs a list of tasks sequentially until first success
     Succeeds if any task succeeds
     """
+
     def __init__(self, task_list):
         GameControlTask.__init__(self)
         self.task_list = task_list
@@ -308,7 +349,9 @@ class SucceedAnyTask(GameControlTask):
 
 class MineSugarTask(GameControlTask):
     """
-    Try to mine sugar by reaching it from one of sides
+    Try to mine sugar by reaching it from one of sides.
+    Has references to a given sugar (by its coordinates) and a given ant (by its id).
+    Also has children tasks to move to target point and mine sugar at point.
     """
 
     def __init__(self, ant_id, sugar_id):
@@ -319,10 +362,14 @@ class MineSugarTask(GameControlTask):
 
     def setup(self):
         any_task_list = []
-        sugar_x, sugar_y = self.f.get_sugar_coords(self.sugar_id)
-        for target_x, target_y in self.f.generate_neighbors(sugar_x, sugar_y):
+        sugar_x, sugar_y = self.game_functions.get_sugar_coords(self.sugar_id)
+        for target_x, target_y in self.game_functions.generate_neighbors(
+            sugar_x, sugar_y, self.ant_id
+        ):
             move_task = MoveAntToTargetPointTask(self.ant_id, target_x, target_y)
-            attack_task = AttackTargetAtPointTask(self.ant_id, sugar_x, sugar_y, self.sugar_id)
+            attack_task = AttackTargetAtPointTask(
+                self.ant_id, sugar_x, sugar_y, self.sugar_id
+            )
             all_task = SucceedAllTask([move_task, attack_task])
             any_task_list.append(all_task)
         self.child_any_task = SucceedAnyTask(any_task_list)
@@ -333,17 +380,17 @@ class MineSugarTask(GameControlTask):
 
     def is_valid(self):
         return not self.child_any_task.has_terminal_state()
-        
+
     def is_succeeded(self):
         return self.child_any_task.is_succeeded()
-    
+
     def run_impl(self):
         self.child_any_task.run()
-        
+
 
 class QueenTask(GameControlTask):
     """
-    Orders ants to mine sugar
+    Orders ants to mine sugar. Runs once per tick.
     """
 
     def __init__(self):
@@ -356,26 +403,30 @@ class QueenTask(GameControlTask):
 
     def create_mine_sugar_task(self, ant):
         sugars = []
-        for sugar in self.f.all_sugars():
+        for sugar in self.game_functions.all_sugars():
             sugars.append(sugar)
         random.shuffle(sugars)
         if len(sugars) > 0:
             mine_sugar_task = MineSugarTask(ant.id, sugars[0].id)
-            mine_sugar_task.update_state(self.f, self.c)
+            mine_sugar_task.update_state(self.game_functions, self.game_play_control)
             mine_sugar_task.setup()
             return mine_sugar_task
         return None
 
     def run_impl(self):
-        for ant in self.f.all_my_ants():
-            if ant.id in self.mine_sugar_tasks and not self.mine_sugar_tasks[ant.id].has_terminal_state():
+        for ant in self.game_functions.all_my_ants():
+            if (
+                ant.id in self.mine_sugar_tasks
+                and not self.mine_sugar_tasks[ant.id].has_terminal_state()
+            ):
+                # This and already has a sugar mining task, run it.
                 self.mine_sugar_tasks[ant.id].run()
             else:
                 mine_sugar_task = self.create_mine_sugar_task(ant)
                 if mine_sugar_task is not None:
                     self.mine_sugar_tasks[ant.id] = mine_sugar_task
 
-        
+
 class RegisterTask(BaseTask):
     """
     Registers client to play a game
@@ -387,7 +438,7 @@ class RegisterTask(BaseTask):
 
     def run_impl(self):
         try:
-            rospy.wait_for_service('/antcolony_server_node/register_cmd', timeout=3.0)
+            rospy.wait_for_service("/antcolony_server_node/register_cmd", timeout=3.0)
         except rospy.ROSException:
             print("ERROR: Wait for service join_cmd failed by timeout")
             self.state = TaskState.FAILED
@@ -397,12 +448,18 @@ class RegisterTask(BaseTask):
             rospy.signal_shutdown("Interrupt")
             exit(1)
         try:
-            register_cmd = rospy.ServiceProxy('/antcolony_server_node/register_cmd', antcolony_msgs.srv.Register)
+            register_cmd = rospy.ServiceProxy(
+                "/antcolony_server_node/register_cmd", antcolony_msgs.srv.Register
+            )
             response = register_cmd(self.player_token)
             if response.status == antcolony_msgs.srv.RegisterResponse.STATUS_OK:
                 self.state = TaskState.SUCCEEDED
             else:
-                print("ERROR: Command to join game failed: {}".format(response.error_message))
+                print(
+                    "ERROR: Command to join game failed: {}".format(
+                        response.error_message
+                    )
+                )
                 self.state = TaskState.FAILED
 
         except rospy.ServiceException as e:
@@ -410,9 +467,7 @@ class RegisterTask(BaseTask):
             self.state = TaskState.FAILED
 
 
-
 class PlayGameTask(BaseTask):
-
     def __init__(self, player_token, player_id):
         BaseTask.__init__(self)
         self.game_state = None
@@ -435,7 +490,7 @@ class PlayGameTask(BaseTask):
         order.actions = control.get_actions()
         if not self.play_service_available:
             try:
-                rospy.wait_for_service('/antcolony_server_node/play_cmd', timeout=3.0)
+                rospy.wait_for_service("/antcolony_server_node/play_cmd", timeout=3.0)
             except rospy.ROSException:
                 print("ERROR: Wait for service play_cmd failed by timeout")
                 self.state = TaskState.FAILED
@@ -447,10 +502,16 @@ class PlayGameTask(BaseTask):
                 exit(1)
             self.play_service_available = True
         try:
-            play_cmd = rospy.ServiceProxy('/antcolony_server_node/play_cmd', antcolony_msgs.srv.Play)
+            play_cmd = rospy.ServiceProxy(
+                "/antcolony_server_node/play_cmd", antcolony_msgs.srv.Play
+            )
             response = play_cmd(order)
             if response.status != antcolony_msgs.srv.PlayResponse.STATUS_OK:
-                print("ERROR: Command to play game failed: {}".format(response.error_message))
+                print(
+                    "ERROR: Command to play game failed: {}".format(
+                        response.error_message
+                    )
+                )
 
         except rospy.ServiceException as e:
             print("ERROR: play_cmd service call failed: %s" % e)
@@ -463,17 +524,30 @@ class PlayGameTask(BaseTask):
 
 
 def game_state_callback(game_state):
+    """Rus once per tick."""
     global play_game_task
     play_game_task.set_game_state(game_state)
     play_game_task.run()
-    if game_state.tick == 999:
+    if game_state.tick == 499:
         rospy.signal_shutdown("INFO: Game is over")
 
 
-if __name__ == '__main__':
-    rospy.init_node('antcolony_client_{}'.format(str(uuid.uuid4())[0:8]))
-    player_token = os.environ.get('ANTCOLONY_PLAYER_TOKEN')
-    player_id = int(os.environ.get('ANTCOLONY_PLAYER_ID'))
+if __name__ == "__main__":
+    rospy.init_node("antcolony_client_{}".format(str(uuid.uuid4())[0:8]))
+    player_token = os.environ.get("ANTCOLONY_PLAYER_TOKEN")
+    player_id = int(os.environ.get("ANTCOLONY_PLAYER_ID"))  # type: ignore
+    print("Env setting: %s" % os.environ.get("ANTCOLONY_USE_CLOSEST_NEIGHBOR"))
+    CONF_USE_CLOSEST_NEIGHBOR = (
+        False
+        if not os.environ.get("ANTCOLONY_USE_CLOSEST_NEIGHBOR")
+        and (
+            str(os.environ.get("ANTCOLONY_USE_CLOSEST_NEIGHBOR")).lower()
+            in ["0", "false", "no", "none"]
+        )
+        else True
+    )
+
+    print("Ants going for closest neighbor: %s" % CONF_USE_CLOSEST_NEIGHBOR)
 
     # Registering for the game
     register_task = RegisterTask(player_token)
--- antcolony_server/include/antcolony_server/antcolony_server.h
+++ antcolony_server/include/antcolony_server/antcolony_server.h
@@ -18,12 +18,12 @@
 #include <antcolony_msgs/State.h>
 #include <antcolony_msgs/Sugar.h>
 
-#include <antcolony_msgs/Register.h>
 #include <antcolony_msgs/Play.h>
+#include <antcolony_msgs/Register.h>
 
 #define MAP_SIZE 40
 
-#define END_TICK 1000
+#define END_TICK 500
 
 #define ANT_ATTACK_HIT 1
 #define SUGAR_ATTACK_HIT 1
@@ -48,157 +48,160 @@
 
 #define GAME_TICK_FREQUENCY 4
 
-
 class GameEngine {
 public:
+  // Constructs game engine
+  GameEngine();
 
-    // Constructs game engine
-    GameEngine();
+  // Gets current state of the game
+  void getCurrentState(antcolony_msgs::State &state);
 
-    // Gets current state of the game
-    void getCurrentState(antcolony_msgs::State &state);
+  // Sets initial state of the game
+  void setInitialState(antcolony_msgs::State &state, int player_one_id,
+                       std::string token_one, int player_two_id,
+                       std::string token_two);
 
-    // Sets initial state of the game
-    void setInitialState(antcolony_msgs::State &state, int player_one_id, std::string token_one,
-                         int player_two_id, std::string token_two);
-
-    void playGameTick(std::vector<antcolony_msgs::Order> &orders);
+  void playGameTick(std::vector<antcolony_msgs::Order> &orders);
 
 private:
-    // Last assigned unique identifier
-    int last_id = 0;
-    int game_tick = 0;
-    int max_coordinate;
+  // Last assigned unique identifier
+  int last_id = 0;
+  int game_tick = 0;
+  int max_coordinate;
 
-    // Security tokens of players
-    std::unordered_map<std::string, int> active_tokens;
+  // Security tokens of players
+  std::unordered_map<std::string, int> active_tokens;
 
-    // Alive ants
-    std::unordered_map<int, antcolony_msgs::Ant> ants;
-    // Alive hives
-    std::unordered_map<int, antcolony_msgs::Hive> hives;
-    // Game players
-    std::unordered_map<int, antcolony_msgs::Player> players;
-    // Available sugars
-    std::unordered_map<int, antcolony_msgs::Sugar> sugars;
+  // Alive ants
+  std::unordered_map<int, antcolony_msgs::Ant> ants;
+  // Alive hives
+  std::unordered_map<int, antcolony_msgs::Hive> hives;
+  // Game players
+  std::unordered_map<int, antcolony_msgs::Player> players;
+  // Available sugars
+  std::unordered_map<int, antcolony_msgs::Sugar> sugars;
 
-    // Baby ants that grow
-    std::vector<antcolony_msgs::Ant> baby_ants;
+  // Baby ants that grow
+  std::vector<antcolony_msgs::Ant> baby_ants;
 
-    // Map occupancy
-    std::unordered_map<int, int> occupancy_map;
+  // Map occupancy
+  std::unordered_map<int, int> occupancy_map;
 
-    // Next unique identifier
-    int nextId();
+  // Next unique identifier
+  int nextId();
 
-    // Rewinds unique identifier behind used id
-    void rewindId(int used_id);
+  // Rewinds unique identifier behind used id
+  void rewindId(int used_id);
 
-    // Convert coordinates to hash
-    int coordinateHash(int x, int y) const;
+  // Convert coordinates to hash
+  int coordinateHash(int x, int y) const;
 
-    // Check if cell of map is occupied
-    bool isOccupied(int x, int y) const;
+  // Check if cell of map is occupied
+  bool isOccupied(int x, int y) const;
 
-    // Generates neighbors for coordinates
-    void generateNeighbors(int x, int y, std::vector<std::pair<int, int>> &neighbors) const;
+  // Generates neighbors for coordinates
+  void generateNeighbors(int x, int y,
+                         std::vector<std::pair<int, int>> &neighbors) const;
 
-    // Find next move on the way from start to end
-    bool searchPath(int start_x, int start_y, int end_x, int end_y, int &next_x, int &next_y) const;
+  // Find next move on the way from start to end
+  bool searchPath(int start_x, int start_y, int end_x, int end_y, int &next_x,
+                  int &next_y) const;
 
-    // If hive has enough sugar start grow baby ant
-    void conceiveBabyAnts();
+  // If hive has enough sugar start grow baby ant
+  void conceiveBabyAnts();
 
-    // Increase health of baby ants on each move
-    void growBabyAnts();
+  // Increase health of baby ants on each move
+  void growBabyAnts();
 
-    void nearestUnoccupied(int start_x, int start_y, int &free_x, int &free_y) const;
+  void nearestUnoccupied(int start_x, int start_y, int &free_x,
+                         int &free_y) const;
 
-    // Spawn grown ant on map
-    void spawnAnt(const int player_id);
+  // Spawn grown ant on map
+  void spawnAnt(const int player_id);
 
-    // Manhattan distance
-    int manhattan_distance(int x1, int y1, int x2, int y2) const;
+  // Manhattan distance
+  int manhattan_distance(int x1, int y1, int x2, int y2) const;
 
-    // Attacks at object at given coordinates
-    void attackTarget(int ant_id, int target_x, int target_y);
+  // Attacks at object at given coordinates
+  void attackTarget(int ant_id, int target_x, int target_y);
 
-    void attackHive(int target_id);
+  void attackHive(int target_id);
 
-    void attackAnt(int target_id);
+  void attackAnt(int target_id);
 
-    void attackSugar(int player_id, int target_id);
+  void attackSugar(int player_id, int target_id);
 
-    void moveAnt(int ant_id, int target_x, int target_y);
+  void moveAnt(int ant_id, int target_x, int target_y);
 
-    void processPlayerOrders(std::vector<antcolony_msgs::Order> &orders);
+  void processPlayerOrders(std::vector<antcolony_msgs::Order> &orders);
 };
 
 class GameServer {
 public:
-    // Initializes game server
-    void init();
+  // Initializes game server
+  void init();
 
-    // Plays one game
-    void playGame();
+  // Plays one game
+  void playGame();
 
 private:
-    // Tokens of registered clients
-    std::unordered_set<std::string> registered_clients;
+  // Tokens of registered clients
+  std::unordered_set<std::string> registered_clients;
 
-    // Mutex to synchronize access to orders
-    std::mutex orchestration_mutex;
+  // Mutex to synchronize access to orders
+  std::mutex orchestration_mutex;
 
-    // Latest orders received for current game tick
-    std::unordered_map<std::string, antcolony_msgs::Order> tick_orders;
+  // Latest orders received for current game tick
+  std::unordered_map<std::string, antcolony_msgs::Order> tick_orders;
 
-    std::string player_one_token;
-    std::string player_two_token;
+  std::string player_one_token;
+  std::string player_two_token;
 
-    int player_one_id;
-    int player_two_id;
+  int player_one_id;
+  int player_two_id;
 
-    antcolony_msgs::State state;
+  antcolony_msgs::State state;
 
-    GameEngine gameEngine;
+  GameEngine gameEngine;
 
-    ros::NodeHandle nh;
-    std::shared_ptr<image_transport::ImageTransport> image_transport;
+  ros::NodeHandle nh;
+  std::shared_ptr<image_transport::ImageTransport> image_transport;
 
-    // Publisher of game board image
-    image_transport::Publisher game_board_image_pub;
+  // Publisher of game board image
+  image_transport::Publisher game_board_image_pub;
 
-    // Publisher of game state
-    ros::Publisher game_state_pub;
+  // Publisher of game state
+  ros::Publisher game_state_pub;
 
-    ros::ServiceServer register_service;
-    ros::ServiceServer play_service;
+  ros::ServiceServer register_service;
+  ros::ServiceServer play_service;
 
-    // Game graphics is here
-    cv::Mat blue_ant_miniature;
-    cv::Mat red_ant_miniature;
-    cv::Mat blue_hive_miniature;
-    cv::Mat red_hive_miniature;
-    cv::Mat white_sugar_miniature;
+  // Game graphics is here
+  cv::Mat blue_ant_miniature;
+  cv::Mat red_ant_miniature;
+  cv::Mat blue_hive_miniature;
+  cv::Mat red_hive_miniature;
+  cv::Mat white_sugar_miniature;
 
-    // Callback to handle requests to register for a new game match
-    bool register_call(antcolony_msgs::RegisterRequest &request, antcolony_msgs::RegisterResponse &response);
+  // Callback to handle requests to register for a new game match
+  bool register_call(antcolony_msgs::RegisterRequest &request,
+                     antcolony_msgs::RegisterResponse &response);
 
-    // Callback to submit an order for the current game tick with exclusive access
-    bool play_call(antcolony_msgs::PlayRequest &request, antcolony_msgs::PlayResponse &response);
+  // Callback to submit an order for the current game tick with exclusive access
+  bool play_call(antcolony_msgs::PlayRequest &request,
+                 antcolony_msgs::PlayResponse &response);
 
-    // Copies orders with exclusive access
-    void copyTickOrders(std::vector<antcolony_msgs::Order> &orders);
+  // Copies orders with exclusive access
+  void copyTickOrders(std::vector<antcolony_msgs::Order> &orders);
 
-    // Prints out final game scores
-    void reportGameScores(antcolony_msgs::State &state);
+  // Prints out final game scores
+  void reportGameScores(antcolony_msgs::State &state);
 
-    // Generates and publishes image of game map
-    void publishGameMapImage();
+  // Generates and publishes image of game map
+  void publishGameMapImage();
 
-    // Sets game of two players
-    void setupGame();
+  // Sets game of two players
+  void setupGame();
 };
 
-
-#endif //ANTCOLONY_SERVER_ANTCOLONY_SERVER_H
+#endif // ANTCOLONY_SERVER_ANTCOLONY_SERVER_H
